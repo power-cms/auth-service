787127adeba71da40fbc054b462b56aa92462449
Szymon Piecuch <szymek.piecuch@gmail.com>
2019-01-14 21:52:05 +0100

    WIP

--- lib/src/application/service/service.spec.ts
+++ lib/src/application/service/service.spec.ts
@@ -1,18 +1,11 @@
 import { IContainer } from '@power-cms/common/application';
-import MongoMemoryServer from 'mongodb-memory-server';
 import { createContainer } from '../../infrastructure/awilix.container';
 import { AuthService } from './service';
 
 describe('Service', () => {
   let container: IContainer;
-  let mongo: MongoMemoryServer;
 
   beforeAll(async () => {
-    mongo = new MongoMemoryServer();
-    process.env.DB_HOST = 'localhost';
-    process.env.DB_PORT = String(await mongo.getPort());
-    process.env.DB_DATABASE = await mongo.getDbName();
-
     container = await createContainer();
   });
 
